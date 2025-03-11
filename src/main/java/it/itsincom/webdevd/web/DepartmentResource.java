ebe049ef6f60e7ddd0527af037313fdfac8859e3
AlessandroLimongi <alessandro.limongi@itsincom.it>
2025-03-11 14:22:34 +0100

    Added functionality to display visits based on selected data

--- src/main/java/it/itsincom/webdevd/web/DepartmentResource.java
+++ src/main/java/it/itsincom/webdevd/web/DepartmentResource.java
@@ -2,19 +2,32 @@ package it.itsincom.webdevd.web;
 
 import io.quarkus.qute.Template;
 import io.quarkus.qute.TemplateInstance;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
+import it.itsincom.webdevd.service.DepartmentService;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @Path("department")
+@Produces(MediaType.TEXT_HTML)
+@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
 public class DepartmentResource {
 
     private final Template department;
+
+    @Inject
+    DepartmentService departmentService;
+
     public DepartmentResource(Template department) {
         this.department = department;
     }
 
     @GET
-    public TemplateInstance showDepartmentPage() {
-        return department.instance();
+    public TemplateInstance showDepartmentPage(@QueryParam("data") String data) {
+        LocalDate date = (data != null && !data.isEmpty()) ? LocalDate.parse(data) : LocalDate.now();
+        List<Visita> visite = departmentService.getVisiteByDate(date);
+        return department.data("visite", visite, "dataSelezionata", date);
     }
 }
