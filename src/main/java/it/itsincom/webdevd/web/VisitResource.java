f7cf3fc6ec55994ace44701160806c36f0f76c25
Alessia Nicolini <alessia.nicolini@itsincom.it>
2025-03-12 11:43:50 +0100

    added exist and saveVisit method

--- src/main/java/it/itsincom/webdevd/web/VisitResource.java
+++ src/main/java/it/itsincom/webdevd/web/VisitResource.java
@@ -8,14 +8,15 @@ import jakarta.ws.rs.Path;
 import jakarta.ws.rs.core.Response;
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.time.LocalDate;
 import java.time.LocalTime;
-
+import java.util.List;
 
 
 @Path("new-visit")
 public class VisitResource {
-    private static final String FILE_PATH = "data/visit.csv";
 
     private final Template visit;
 
@@ -29,7 +30,7 @@ public class VisitResource {
     }
 
     @POST
-    public Response writeVisit(@FormParam("mail") String mail,
+    public Response writeVisit(@FormParam("email") String email,
                                @FormParam("date") LocalDate date,
                                @FormParam("start") LocalTime start,
                                @FormParam("duration") int duration
@@ -39,7 +40,7 @@ public class VisitResource {
         String FILE_PATH = "data/visit.csv";
         LocalTime end = start.plusMinutes(duration);
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(FILE_PATH))) {
-            writer.write(STR."\{mail},\{date},\{start},\{end}");
+            writer.write(STR."\{email},\{date},\{start},\{end}");
             writer.newLine();
             return Response.ok("Dati salvati correttamente!").build();
         } catch (IOException e) {
@@ -47,7 +48,27 @@ public class VisitResource {
         }
     }
 
+    public boolean exist(String email) throws IOException {
+        String FILE_PATH = "data/visitors.csv";
+        List<String> lines = Files.readAllLines(Paths.get(FILE_PATH));
+        for (String line : lines) {
+            String[] parts = line.split(",");
+            if (parts[3].equals(email)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public Response saveVisit(String email, LocalDate date, LocalTime start, int duration) throws IOException {
+        if (exist(email)) {
+            return writeVisit(email, date, start, duration);
+        }
+        return Response.status(500).entity("L'utente non è registrato").build();
+    }
 }
+
+
 
 
 
