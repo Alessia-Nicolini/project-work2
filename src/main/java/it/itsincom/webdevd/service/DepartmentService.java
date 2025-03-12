7a6d127bb0e5528619f8bc4a2b1b7c4d2a83d66b
AlessandroLimongi <alessandro.limongi@itsincom.it>
2025-03-12 12:05:52 +0100

    Retrieve visits from a CSV file by filtering by date**

--- src/main/java/it/itsincom/webdevd/service/DepartmentService.java
+++ src/main/java/it/itsincom/webdevd/service/DepartmentService.java
@@ -2,14 +2,35 @@ package it.itsincom.webdevd.service;
 
 import jakarta.enterprise.context.ApplicationScoped;
 import it.itsincom.webdevd.model.Visita;
-import java.time.format.DateTimeFormatter;
 
+import java.io.*;
+import java.nio.file.*;
+import java.time.LocalDate;
+import java.util.*;
 
 @ApplicationScoped
 public class DepartmentService {
     private static final String VISITE_FILE = "src/main/resources/visit.csv";
-    private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+    public List<Visita> getVisiteByDate(LocalDate data) {
+        List<Visita> visite = new ArrayList<>();
+
+        try (BufferedReader reader = Files.newBufferedReader(Paths.get(VISITE_FILE))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                Visita visita = Visita.fromCSV(line);
 
 
+                if (visita != null && visita.date.equals(data)) {
+                    visite.add(visita);
+                }
+            }
+        } catch (IOException e) {
+
+            e.printStackTrace();
+        }
+
+        return visite;
+    }
 
 }
