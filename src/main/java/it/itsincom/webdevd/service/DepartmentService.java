fdd6f8fa6e1578acec03bcd355ea44d6e4b27525
AlessandroLimongi <alessandro.limongi@itsincom.it>
2025-03-12 10:47:09 +0100

    Defining constants for managing visits

--- src/main/java/it/itsincom/webdevd/service/DepartmentService.java
+++ src/main/java/it/itsincom/webdevd/service/DepartmentService.java
@@ -1,6 +1,15 @@
 package it.itsincom.webdevd.service;
 
+import jakarta.enterprise.context.ApplicationScoped;
+import it.itsincom.webdevd.model.Visita;
+import java.time.format.DateTimeFormatter;
 
+
+@ApplicationScoped
 public class DepartmentService {
+    private static final String VISITE_FILE = "src/main/resources/visit.csv";
+    private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+
 
 }
