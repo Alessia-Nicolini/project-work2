74e772c082aec774a3c6741722b2d526d4134553
Alessia Nicolini <alessia.nicolini@itsincom.it>
2025-03-13 09:58:43 +0100

    riadded exist and saveVisit method

--- src/main/java/it/itsincom/webdevd/web/NewVisitResource.java
+++ src/main/java/it/itsincom/webdevd/web/NewVisitResource.java
@@ -9,9 +9,11 @@ import jakarta.ws.rs.Path;
 import jakarta.ws.rs.core.Response;
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.time.LocalDate;
 import java.time.LocalTime;
-
+import java.util.List;
 
 
 @Path("newVisit")
@@ -29,6 +31,9 @@ public class NewVisitResource {
     }
 
     @POST
+
+
+
     public Response writeVisit(@FormParam("mail") String mail,
                                @FormParam("date") LocalDate date,
                                @FormParam("start") LocalTime start,
@@ -46,8 +51,27 @@ public class NewVisitResource {
             return Response.status(500).entity("Errore nel salvataggio dei dati").build();
         }
     }
+    public boolean exist(String email) throws IOException {
+        String FILE_PATH = "data/visitors.csv";
+        List<String> lines = Files.readAllLines(Paths.get(FILE_PATH));
+        for (String line : lines) {
+            String[] parts = line.split(",");
+            if (parts[3].equals(email)) {
+                return true;
+            }
+        }
+        return false;
+    }
 
+    public Response saveVisit(String email, LocalDate date, LocalTime start, int duration) throws IOException {
+        if (exist(email)) {
+            return writeVisit(email, date, start, duration);
+        }
+        return Response.status(500).entity("L'utente non è registrato").build();
+    }
 }
+
+
 
 
 
