4be3fe3ec411c4872aff1821a674f1b4e7a47b73
Martina <martina.mizza@gmail.comgit config --global user.name Martinacleargit config --global user.name Martinagit config --global user.email martina.mizza@gmail.com>
2025-03-19 10:47:41 +0100

    create NewVisitorService.java

---
+++ src/main/java/it/itsincom/webdevd/services/NewVisitorService.java
@@ -0,0 +1,40 @@
+package it.itsincom.webdevd.services;
+import it.itsincom.webdevd.model.Visitor;
+import it.itsincom.webdevd.repositories.NewVisitorRepository;
+import jakarta.enterprise.context.ApplicationScoped;
+import java.util.List;
+
+@ApplicationScoped
+public class NewVisitorService {
+    public static final String OPERATION_SUCCESS = "success";
+    private final NewVisitorRepository newVisitorRepository;
+
+    public NewVisitorService(NewVisitorRepository newVisitorRepository) {
+        this.newVisitorRepository = newVisitorRepository;
+    }
+    public String addNewVisitor(String name, String surname, String phone, String email) {
+        if (name == null || name.isEmpty() || surname == null || surname.isEmpty() ||
+                phone == null || phone.isEmpty() || email == null || email.isEmpty()) {
+            return "Tutti i campi sono obbligatori.";
+        }
+
+        List<Visitor> visitors = newVisitorRepository.getAllVisitors();
+
+        for (Visitor visitor : visitors) {
+            if (visitor.getName().equalsIgnoreCase(name) &&
+                    visitor.getSurname().equalsIgnoreCase(surname) &&
+                    visitor.getPhone().equals(phone) &&
+                    visitor.getEmail().equals(email)) {
+                return "Il visitatore è già registrato!";
+            }
+        }
+
+        Visitor visitor = new Visitor(0, name, surname, phone, email);
+
+        newVisitorRepository.addNewVisitor(visitor);
+        return OPERATION_SUCCESS;
+    }
+}
+
+
+
