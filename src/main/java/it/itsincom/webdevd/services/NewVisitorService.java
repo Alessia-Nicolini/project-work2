40e6d6ebbfdaee3afef7dae55cd89e5becda7ab9
RabbiHossain05 <rabbi.hossain@itsincom.it>
2025-03-19 20:10:58 +0100

    Styled the reception page's table.

--- src/main/java/it/itsincom/webdevd/services/NewVisitorService.java
+++ src/main/java/it/itsincom/webdevd/services/NewVisitorService.java
@@ -1,19 +1,21 @@
 package it.itsincom.webdevd.services;
-import it.itsincom.webdevd.model.Visitor;
+import it.itsincom.webdevd.models.Visitor;
 import it.itsincom.webdevd.repositories.NewVisitorRepository;
 import jakarta.enterprise.context.ApplicationScoped;
 import java.util.List;
 
 @ApplicationScoped
 public class NewVisitorService {
-    public static final String OPERATION_SUCCESS = "success";
+    public static final String OPERATION_SUCCESS = "Success";
+
     private final NewVisitorRepository newVisitorRepository;
 
     public NewVisitorService(NewVisitorRepository newVisitorRepository) {
         this.newVisitorRepository = newVisitorRepository;
     }
-    public String addNewVisitor(String name, String surname, String phone, String email) {
-        if (name == null || name.isEmpty() || surname == null || surname.isEmpty() ||
+
+    public String addNewVisitor(String first_name, String last_name, String email, String phone) {
+        if (first_name == null || first_name.isEmpty() || last_name == null || last_name.isEmpty() ||
                 phone == null || phone.isEmpty() || email == null || email.isEmpty()) {
             return "Tutti i campi sono obbligatori.";
         }
@@ -21,15 +23,15 @@ public class NewVisitorService {
         List<Visitor> visitors = newVisitorRepository.getAllVisitors();
 
         for (Visitor visitor : visitors) {
-            if (visitor.getName().equalsIgnoreCase(name) &&
-                    visitor.getSurname().equalsIgnoreCase(surname) &&
+            if (visitor.getFirstName().equalsIgnoreCase(first_name) &&
+                    visitor.getLastName().equalsIgnoreCase(last_name) &&
                     visitor.getPhone().equals(phone) &&
                     visitor.getEmail().equals(email)) {
                 return "Il visitatore è già registrato!";
             }
         }
 
-        Visitor visitor = new Visitor(0, name, surname, phone, email);
+        Visitor visitor = new Visitor(0, first_name, last_name, email, phone);
 
         newVisitorRepository.addNewVisitor(visitor);
         return OPERATION_SUCCESS;
